7af9a1ebfbcb2f2c358e18cc59c47cec43d5436b
zaiky09 <zamilmozamil@outlook.com>
2022-11-22 07:39:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main#main');
+main.remove('#main');
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Zamil is the champion"
